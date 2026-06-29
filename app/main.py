fe24b099cebb12e15475cca31b96116bdfaa4408
Daniel Mamak <daniel.mamak@gmail.com>
2026-06-29 21:43:58 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,5 +2,26 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    result = {}
+
+    def wrapper(*args, **kwargs) -> None:
+        key = (args, tuple(kwargs.items()))
+        if key not in result:
+            print("Calculating new result")
+            result2 = func(*args, **kwargs)
+            result[key] = result2
+            return result2
+        else:
+            print("Getting from cache")
+            return result[key]
+    return wrapper
+
+
+@cache
+def long_time_func(base: int, exponent: int, power: int) -> int:
+    return (base ** exponent ** power) % (base * power)
+
+
+print(long_time_func(1, 2, 3))
+print(long_time_func(2, 2, 3))
+print(long_time_func(1, 2, 3))
