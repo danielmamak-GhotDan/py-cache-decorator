38a6cae34eef0928dcc2d57a3eb817c388f3d758
Daniel Mamak <daniel.mamak@gmail.com>
2026-06-30 17:54:42 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -27,9 +27,10 @@ def long_time_func_2(n_tuple: tuple, power: int) -> int:
     return [number ** power for number in n_tuple]
 
 
-long_time_func(1, 2, 3)
-long_time_func(2, 2, 3)
-long_time_func_2((5, 6, 7), 5)
-long_time_func(1, 2, 3)
-long_time_func_2((5, 6, 7), 10)
-long_time_func_2((5, 6, 7), 10)
+if __name__ == "__name__":
+    long_time_func(1, 2, 3)
+    long_time_func(2, 2, 3)
+    long_time_func_2((5, 6, 7), 5)
+    long_time_func(1, 2, 3)
+    long_time_func_2((5, 6, 7), 10)
+    long_time_func_2((5, 6, 7), 10)
